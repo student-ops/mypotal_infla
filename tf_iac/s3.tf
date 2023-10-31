294a935f084bd3ee0593dced484b9ff9e9d3daae
student-ops <gogakuec@gmail.com>
2023-11-01 03:08:44 +0900

    update s3 force destroy

--- tf_iac/s3.tf
+++ tf_iac/s3.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "mypotal_static" {
     Name        = "my-potal-bucket"
     Environment = "static"
   }
+  force_destroy = true
 }
 resource "aws_s3_bucket_website_configuration" "my_potal_website" {
   bucket = aws_s3_bucket.mypotal_static.id
